1e29f93a23313f545fcff45ee6cad7a83dbb6de3
tomi <herceg133@gmail.com>
2022-03-01 19:27:11 +0100

    Added functional LoadingScreen

--- Assets/Scripts/LevelLoader.cs
+++ Assets/Scripts/LevelLoader.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using UnityEngine.SceneManagement;
 using UnityEngine;
+using TMPro;
 using UnityEngine.UI;
 
 public class LevelLoader : MonoBehaviour
@@ -8,29 +9,106 @@ public class LevelLoader : MonoBehaviour
     public GameObject loadingScreen;
     public GameObject mainmenuUI;
     public GameObject garageUI;
+    public GameObject pauseUI;
+    public GameObject HUDUI;
 
     public Slider loadingSlider;
+    public TextMeshProUGUI loadingTitle;
+    public GameObject pressanykeyUI;
+
+    public Sprite[] backgrounds;
+    public Image backgroundImage;
+
+    public TextMeshProUGUI tipsText;
+
+    public string[] tips;
+    public int tipCount;
 
     public void LoadLevel(string sceneName)
     {
-        garageUI.SetActive(false);
-        mainmenuUI.SetActive(false);
+        backgroundImage.sprite = backgrounds[Random.Range(0, backgrounds.Length)];
+
+        #region Debugs for Exceptions
+        if (garageUI)
+        {
+            garageUI.SetActive(false);
+        }
+
+        if (mainmenuUI)
+        {
+            mainmenuUI.SetActive(false);
+        }
+
+        if (pauseUI)
+        {
+            pauseUI.SetActive(false);
+        }
+
+        if (HUDUI)
+        {
+            HUDUI.SetActive(false);
+        }
+
+        #endregion
+
         loadingScreen.SetActive(true);
+        loadingTitle.text = sceneName;
         StartCoroutine(LoadAsynchronously(sceneName));
+        StartCoroutine(GenerateTips());
+
     }
 
     IEnumerator LoadAsynchronously(string sceneName)
     {
         AsyncOperation operation = SceneManager.LoadSceneAsync(sceneName);
-        
+
+        operation.allowSceneActivation = false;
 
         while (!operation.isDone)
         {
             float progress = Mathf.Clamp01(operation.progress / 0.9f);
             loadingSlider.value = progress;
-            Debug.Log(progress);
+
+            if (progress >= .9f && !operation.allowSceneActivation)
+            {
+                pressanykeyUI.SetActive(true);
+
+                if (Input.anyKey)
+                {
+                    operation.allowSceneActivation = true;
+                }
+            }
+            //Debug.Log(progress);
 
             yield return null;
+
+        }
+
+    }
+
+    public IEnumerator GenerateTips()
+    {
+        tipCount = Random.Range(0, tips.Length);
+        tipsText.text = tips[tipCount];
+
+        //Debug.Log(loadingScreen.activeInHierarchy);
+
+        while (loadingScreen.activeInHierarchy)
+        {
+
+            yield return new WaitForSeconds(8f);
+
+            //Make transition
+
+            //yield return new WaitForSeconds(0.5f);
+
+            tipCount++;
+            if (tipCount >= tips.Length)
+            {
+                tipCount = 0;
+            }
+
+            tipsText.text = tips[tipCount];
 
         }
 
